cd7e2faa28458374d06eda34cfaa803799ccfe0d
심상갑 <kappa2410@gmail.com>
2024-07-09 17:27:04 +0900

    Feat: doc_img - example 에 이미지 추가

--- FlowLayoutViewDemo/FlowLayoutViewDemo/ContentView.swift
+++ FlowLayoutViewDemo/FlowLayoutViewDemo/ContentView.swift
@@ -11,30 +11,29 @@ import GabFlowLayout
 
 struct ContentView: View {
     
+    var list: [String] = [
+        "Sim",
+        "Sang",
+        "Gab",
+        "KaPPa",
+        "GabFlowLayout",
+        "GabChipView",
+        "swiftui-flowLayout",
+        "Gab's 4th SwiftUI Library"
+    ]
+    
     var body: some View {
-        
         FlowLayoutView(.horizontal) {
-            Text("222")
-                .frame(width: 30, height: 30)
-                .background(.white)
-            
-            Text("333333")
-                .font(.system(size: 20))
-                .background(Color.white)
-            
-            Text("444444444444")
-                .font(.system(size: 20))
-                .frame(height: 500, alignment: .top)
-            
-            Rectangle()
-                .fill(.gray)
-                .frame(width: 100, height: 100)
-            
-            Rectangle()
-                .fill(.gray)
-                .frame(width: 100, height: 100)
+            ForEach(list, id: \.self) { string in
+                Text(string)
+                    .font(.headline)
+                    .padding(.all, 2)
+                    .background(.mint)
+                    .cornerRadius(8)
+            }
         }
         .configurationSpacing(line: 5, item: 5)
+        .padding(.all, 10)
     }
 }
 
